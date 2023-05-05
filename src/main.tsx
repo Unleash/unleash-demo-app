cca1f46244dbc42abd168fa843e2246feea2f442
Nuno Góis <github@nunogois.com>
2023-05-05 14:44:44 +0100

    point to nuno sandbox instance

--- src/main.tsx
+++ src/main.tsx
@@ -14,9 +14,9 @@ if (!userId) {
 }
 
 const config: IConfig = {
-  url: 'http://localhost:3000/api/frontend',
+  url: 'https://sandbox.getunleash.io/nuno/api/frontend',
   clientKey:
-    'demo-app:dev.ab507a137aafee28be588844b0ab6db238ba2f3295c1b650bf365619',
+    'demo-app:dev.f2223d96498eb61d2810e1951fd107e9fc663b54e1ac4f1543673596',
   refreshInterval: 2,
   appName: 'unleash-demo-app',
   context: { userId }
