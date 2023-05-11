d5f2c464831cde31a2fb810ef2354dd11530a095
Nuno Góis <github@nunogois.com>
2023-05-11 16:27:51 +0100

    fix: update banner to specify env

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => (
   <div className='hidden sm:flex sticky top-0 bg-web text-white h-12 items-center justify-center gap-4'>
-    This demo is controlled by Unleash.
+    This demo is controlled by Unleash (dev environment).
     <a
       href='https://app.unleash-hosted.com/demo'
       target='_blank'
